5f1432eb091a0523fb248b126861a06153d1e494
FadhilD <mefx226@gmail.com>
2025-06-10 13:12:32 +0700

    1

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,9 @@
+// src/api/axiosInstance.js
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://tcc-kelompok9-be-995193249744.us-central1.run.app/", // Ganti dengan base URL backend-mu
+  withCredentials: true, // untuk kirim cookie kalau dibutuhkan
+});
+
+export default instance;
